54a2edc35a8c59dcca68a1fb70fa354ad4036a86
Jeff Standen <jeff@webgroupmedia.com>
2012-08-22 18:53:39 -0700

    6.1 compatibility

--- api/dao/datacenter_sensor.php
+++ api/dao/datacenter_sensor.php
@@ -350,20 +350,16 @@ class DAO_DatacenterSensor extends C4_ORMHelper {
 		// Translate virtual fields
 		
 		$args = array(
-			'join_sql' => $join_sql,
-			'where_sql' => $where_sql,
-			'has_multiple_values' => $has_multiple_values
+			'join_sql' => &$join_sql,
+			'where_sql' => &$where_sql,
+			'has_multiple_values' => &$has_multiple_values
 		);
 		
 		array_walk_recursive(
 			$params,
 			array('DAO_DatacenterSensor', '_translateVirtualParameters'),
-			&$args
+			$args
 		);
-		
-		$join_sql = $args['join_sql'];
-		$where_sql = $args['where_sql'];
-		$has_multiple_values = $args['has_multiple_values'];
 		
 		return array(
 			'primary_table' => 'datacenter_sensor',
