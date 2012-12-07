046ad1cf4fb2dddf41bd35ad3808fc54b8078cb9
Jeff Standen <jeff@webgroupmedia.com>
2012-12-06 19:54:30 -0800

    [6.2 compatibility] 'Set links' action in VA events

--- api/events/abstract_event_sensor.php
+++ api/events/abstract_event_sensor.php
@@ -278,7 +278,7 @@ abstract class AbstractEvent_Sensor extends Extension_DevblocksEvent {
 				'create_task' => array('label' =>'Create a task'),
 				'create_ticket' => array('label' =>'Create a ticket'),
 				'schedule_behavior' => array('label' => 'Schedule behavior'),
-				'set_sensor_links' => array('label' => 'Set links on sensor'),
+				'set_links' => array('label' => 'Set links'),
 				'unschedule_behavior' => array('label' => 'Unschedule behavior'),
 			)
 			+ DevblocksEventHelper::getActionCustomFields('cerberusweb.contexts.datacenter.sensor')
@@ -322,8 +322,8 @@ abstract class AbstractEvent_Sensor extends Extension_DevblocksEvent {
 				$dates = array();
 				$conditions = $this->getConditions($trigger);
 				foreach($conditions as $key => $data) {
-					if($data['type'] == Model_CustomField::TYPE_DATE)
-					$dates[$key] = $data['label'];
+					if(isset($data['type']) && $data['type'] == Model_CustomField::TYPE_DATE)
+						$dates[$key] = $data['label'];
 				}
 				$tpl->assign('dates', $dates);
 			
@@ -334,10 +334,8 @@ abstract class AbstractEvent_Sensor extends Extension_DevblocksEvent {
 				DevblocksEventHelper::renderActionUnscheduleBehavior($trigger);
 				break;
 				
-			case 'set_sensor_links':
-				$contexts = Extension_DevblocksContext::getAll(false);
-				$tpl->assign('contexts', $contexts);
-				$tpl->display('devblocks:cerberusweb.core::events/action_set_links.tpl');
+			case 'set_links':
+				DevblocksEventHelper::renderActionSetLinks($trigger);
 				break;
 				
 			default:
@@ -351,7 +349,7 @@ abstract class AbstractEvent_Sensor extends Extension_DevblocksEvent {
 		
 		$tpl->clearAssign('params');
 		$tpl->clearAssign('namePrefix');
-		$tpl->clearAssign('token_labels');		
+		$tpl->clearAssign('token_labels');
 	}
 	
 	function simulateActionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
@@ -389,7 +387,8 @@ abstract class AbstractEvent_Sensor extends Extension_DevblocksEvent {
 				return DevblocksEventHelper::simulateActionUnscheduleBehavior($params, $dict, 'sensor_id');
 				break;
 				
-			case 'set_sensor_links':
+			case 'set_links':
+				return DevblocksEventHelper::simulateActionSetLinks($trigger, $params, $dict);
 				break;
 				
 			default:
@@ -410,7 +409,7 @@ abstract class AbstractEvent_Sensor extends Extension_DevblocksEvent {
 					if(!empty($context) && !empty($context_id))
 						return DevblocksEventHelper::simulateActionSetCustomField($custom_field, 'sensor_custom', $params, $dict, $context, $context_id);
 				}
-				break;	
+				break;
 		}
 	}
 	
@@ -449,33 +448,8 @@ abstract class AbstractEvent_Sensor extends Extension_DevblocksEvent {
 				DevblocksEventHelper::runActionUnscheduleBehavior($params, $dict, 'sensor_id');
 				break;
 				
-			case 'set_sensor_links':
-				@$to_context_strings = $params['context_objects'];
-
-				if(!is_array($to_context_strings) || empty($to_context_strings))
-					break;
-
-				$from_context = null;
-				$from_context_id = null;
-				
-				switch($token) {
-					case 'set_sensor_links':
-						$from_context = 'cerberusweb.contexts.datacenter.sensor';
-						@$from_context_id = $dict->sensor_id;
-						break;
-				}
-				
-				if(empty($from_context) || empty($from_context_id))
-					break;
-				
-				foreach($to_context_strings as $to_context_string) {
-					@list($to_context, $to_context_id) = explode(':', $to_context_string);
-					
-					if(empty($to_context) || empty($to_context_id))
-						continue;
-					
-					DAO_ContextLink::setLink($from_context, $from_context_id, $to_context, $to_context_id);
-				}				
+			case 'set_links':
+				DevblocksEventHelper::runActionSetLinks($trigger, $params, $dict);
 				break;
 				
 			default:
@@ -496,7 +470,7 @@ abstract class AbstractEvent_Sensor extends Extension_DevblocksEvent {
 					if(!empty($context) && !empty($context_id))
 						DevblocksEventHelper::runActionSetCustomField($custom_field, 'sensor_custom', $params, $dict, $context, $context_id);
 				}
-				break;	
+				break;
 		}
 	}
 	
