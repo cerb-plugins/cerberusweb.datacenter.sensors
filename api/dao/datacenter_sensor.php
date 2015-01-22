7ba83a8ca1fe1bc9f4ea6529574af72ad567b34d
Jeff Standen <jeff@webgroupmedia.com>
2015-01-21 18:18:11 -0800

    6.9 API compatibility

--- api/dao/datacenter_sensor.php
+++ api/dao/datacenter_sensor.php
@@ -860,6 +860,19 @@ class View_DatacenterSensor extends C4_AbstractView implements IAbstractView_Sub
 		
 		$fields = self::_appendFieldsFromQuickSearchContext(CerberusContexts::CONTEXT_SENSOR, $fields, null);
 		
+		// Engine/schema examples: Comments
+		
+		$ft_examples = array();
+		
+		if(false != ($schema = Extension_DevblocksSearchSchema::get(Search_CommentContent::ID))) {
+			if(false != ($engine = $schema->getEngine())) {
+				$ft_examples = $engine->getQuickSearchExamples($schema);
+			}
+		}
+		
+		if(!empty($ft_examples))
+			$fields['comments']['examples'] = $ft_examples;
+		
 		// Sort by keys
 		
 		ksort($fields);
