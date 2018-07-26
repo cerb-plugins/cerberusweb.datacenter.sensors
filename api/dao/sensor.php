3a23b1e04896e9fb7beb5138dec25dbd7321ea78
Jeff Standen <jeff@webgroupmedia.com>
2018-07-26 13:55:50 -0700

    9.0 compatibility

--- api/dao/sensor.php
+++ api/dao/sensor.php
@@ -585,11 +585,11 @@ class SearchFields_DatacenterSensor extends DevblocksSearchFields {
 		return false;
 	}
 	
-	static function getFieldForSubtotalKey($key, array $query_fields, array $search_fields, $primary_key) {
+	static function getFieldForSubtotalKey($key, $context, array $query_fields, array $search_fields, $primary_key) {
 		switch($key) {
 		}
 		
-		return parent::getFieldForSubtotalKey($key, $query_fields, $search_fields, $primary_key);
+		return parent::getFieldForSubtotalKey($key, $context, $query_fields, $search_fields, $primary_key);
 	}
 	
 	static function getLabelsForKeyValues($key, $values) {
